1d4ae0ecb8ffba2a85e68573d2ef32106a512c0a
Maksud Alam <maksud.patna@gmail.com>
2024-09-10 20:50:53 +0530

    Day 1 Done

---
+++ PS-Sprint-1/1evenOdd.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ int main () {
+ int n;
+ cout<<"Enter mumber :";
+ cin>>n;
+ if(n%2==0)
+  cout<<n<<" is even";
+ else
+  cout<<n<<" is odd";
+return 0;
+}
